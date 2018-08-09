6402aa48d7d8dae930aba529a4e9a69d7a948ef9
Matt Addison <matthew.addison@talk21.com>
2018-08-09 14:17:16 +0100

    completes extensions

--- star_system.rb
+++ star_system.rb
@@ -32,8 +32,28 @@ class StarSystem
   end
 
   def get_number_of_planets_closer_than(number)
-    close_planets = @planets.find_all {|planet| planet.distance_from_sun < number}
-    close_planets.map {|planet| planet.name}.count
+    # close_planets = @planets.find_all {|planet| planet.distance_from_sun < number}
+    # close_planets.map {|planet| planet.name}.count
+    @planets.count {|planet| planet.distance_from_sun < number}
+  end
+
+  def get_total_number_of_moons
+    @planets.reduce(0) {|total, planet| total + planet.number_of_moons}
+  end
+
+  def get_planet_names_sorted_by_increasing_distance_from_sun
+    planets_by_distance = @planets.sort_by {|planet| planet.distance_from_sun}
+    planets_by_distance.map {|planet| planet.name}
+  end
+
+  # def get_planet_names_sorted_by_size_decreasing
+  #   planets_by_size = @planets.sort_by {|planet| planet.diameter}
+  #   planets_by_size.map {|planet| planet.name}.reverse!
+  # end
+
+  def get_planet_names_sorted_by_size_decreasing
+    planets_by_size = @planets.sort { |planet1, planet2| planet2.diameter <=> planet1.diameter }
+    return planets_by_size.map { |planet| planet.name }
   end
 
 end
